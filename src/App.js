7d2aef3e115ca8d7a758ac086eaf5841258602fa
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-03-13 22:01:37 +0600

    google signin added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,75 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
 firebase.initializeApp(firebaseConfig);
 
+
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email:'',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, photoURL, email} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signedInUser);
+      console.log(displayName, email, photoURL);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const signedOutUser = {
+        isSignedIn: false, 
+        name: '',
+        phot:'',
+        email:'',
+        password:'',
+        error:'',
+        isValid:false,
+        existingUser: false
+      }
+      setUser(signedOutUser);
+      console.log(res);
+    })
+    .catch( err => {
+
+    })
+  }
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut} >Sign out</button> :
+        <button onClick={handleSignIn} >Sign in</button>
+      }
+      {
+        user.isSignedIn && <div>
+          <p> Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt=""></img>
+        </div>
+      }
     </div>
   );
 }
