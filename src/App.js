57bd5455b32e1947e06985133e1bad61d2958da7
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-03-13 21:19:28 +0600

    firebase initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>Hello</h1>
     </div>
   );
 }
